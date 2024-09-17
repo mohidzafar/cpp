8125b78a038d7d2ea0eb93237b9e11b5c269afe8
mohidzafar <mohidzafar42@gmail.com>
2024-09-17 22:48:12 +0500

    added code and files

---
+++ 1-learning/1.1-Basics/1.1.2-Comments/main.cpp
@@ -0,0 +1,19 @@
+/* This prgram prints 'Hello World'
+on screen */
+
+
+// iostream library
+#include <iostream>
+
+// Adding namespace std
+using namespace std;
+
+// main function
+int main() {
+
+    // Using cout function to print 'Hello World'
+    cout << "Hello World" << endl;
+
+    //return statement
+    return 0;
+}
