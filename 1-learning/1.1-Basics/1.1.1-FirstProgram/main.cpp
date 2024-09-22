2b99e9587194044c98d100b842882010eb164e9f
mohidzafar <mohidzafar42@gmail.com>
2024-09-22 13:55:03 +0500

    .

--- 1-learning/1.1-Basics/1.1.1-FirstProgram/main.cpp
+++ 1-learning/1.1-Basics/1.1.1-FirstProgram/main.cpp
@@ -4,32 +4,7 @@
 using namespace std;
 
 int main() {
-    float value_1, value_2;
-    char operate;
-    cout << "Enter Calculation: ";
-    cin >> value_1;
-    cin >> operate;
-    cin >> value_2;
-    if (operate == '+')
-    {
-        float sum = value_1 + value_2;
-        cout << "Answer: " << sum << endl;
-    }
-    else if(operate == '-')
-    {
-        float diff = value_1 - value_2;
-        cout << "Answer: " << diff << endl;
-    }
-    else if(operate == '*')
-    {
-        float product = value_1 * value_2;
-        cout << "Answer: " << product << endl;
-    }
-    else if(operate == '/')
-    {
-        float divide = value_1 / value_2;
-        cout << "Answer: " << divide << endl;
-    }
-    
+    cout << "My name is Mohid Zafar" << endl;
+    cout << "I am 15 years old" << endl;
     return 0;
 }
---
+++ 3-projects/3.3.Age-Calculator/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void age_calculator() {
+    int b_year;
+    int b_month;
+    int b_date;
+
+    int year;
+    int month;
+    int date;
+
+    cout << "Enter Your Birth Year: ";
+    cin >> b_year;
+    cout << "Enter Your Birth Month: ";
+    cin >> b_month;
+    cout << "Enter Your Birth Date: ";
+    cin >> b_date;
+
+    year = 2024 - b_year;
+    month = 9 - b_month;
+    date = 22 - b_date;
+
+    if(month < 0) {
+        month = !month;
+    }
+    if(date < 0) {
+        date = !date;
+    }
+
+    cout << "You are " << year << " years, " << month << " months and " << date << " days old" << endl; 
+}
+
+int main() {
+    age_calculator();
+    return 0;
+}
